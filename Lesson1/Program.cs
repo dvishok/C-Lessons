d6901f4fb2db4d527cfbc4673b90b27e0deddacd
dvish <dvishnevetsky2005@gmail.com>
2022-04-17 20:39:54 +0300

    Lesson2 added

--- Lesson1/Program.cs
+++ Lesson1/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int ch1,ch2,sum;
+Console.Write("Введите число: ");
+ch1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Первое число = " + ch1);
+Console.Write("Введите число: ");
+ch2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Второе число = " + ch2);
+sum=ch1+ch2;
+Console.WriteLine(ch1 + "+" + ch2 + "=" + sum);
